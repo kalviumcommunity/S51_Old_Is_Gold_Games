e5d8407474790719938980f737fa7f5f164b091b
Zion203 <jesudas.t@kalvium.community>
2024-02-19 23:14:08 +0530

    db.js is added

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+require("dotenv").config()
+
+const startDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log('connected to MongoDB');
+  } catch (err) {
+    console.error('error connecting to MongoDB:', err.message);
+  }
+};
+
+const stopDatabase = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log('disconnected from MongoDB');
+  } catch (err) {
+    console.error('error disconnecting from MongoDB:', err.message);
+  }
+};
+
+const isConnected = () => {
+  return mongoose.connection.readyState === 1;
+}
+
+module.exports = { startDatabase, stopDatabase, isConnected };
